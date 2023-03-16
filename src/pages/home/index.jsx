adcdd0e2c5aa69110c66a6f61f9cf3272599384c
MATEUS FELIPE DIAS <0058564@IFMG.EDU.BR>
2023-03-16 17:43:37 -0300

    quiz

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './Styles.css';
+import LogoQuiz from '../../assets/images/quiz.png'
+
+function Home(){
+    return(
+        <div className="container">
+            <h1 className = 'title'>Bem Vindo ao </h1>
+            <img src = {LogoQuiz} alt= 'Logo' className="Logo" />
+            <small  className = "description"> Teste seus conhecimentos sobre react</small>
+            <button className = "button-start">🚀Começar</button>
+            <footer className = 'footer'>
+                <span className = 'author'> criado por <a href="https://github.com/mateus00dias" target = "_blank" rel = "noreferrer" className = 'author'>Mateus Dias</a></span>
+            </footer>
+        </div>
+    )
+}
+
+export default Home;
