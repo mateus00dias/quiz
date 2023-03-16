5878ab78071956dece135197a1ed21e5f1ee74c0
Mateus Dias <mfdprofissional@gmail.com>
2023-03-16 20:32:42 -0300

    conserto

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -6,11 +6,11 @@ function Home(){
     return(
         <div className="container">
             <h1 className = 'title'>Bem Vindo ao </h1>
-            <img src = {LogoQuiz} alt= 'Logo' className="Logo" />
-            <small  className = "description"> Teste seus conhecimentos sobre react</small>
-            <button className = "button-start">🚀Começar</button>
+            <img src = {LogoQuiz} alt= 'Logo' className='logo' />
+            <small  className = 'description'> Teste seus conhecimentos sobre react</small>
+            <button className = 'button-start'>🚀Começar</button>
             <footer className = 'footer'>
-                <span className = 'author'> criado por <a href="https://github.com/mateus00dias" target = "_blank" rel = "noreferrer" className = 'author'>Mateus Dias</a></span>
+                <span className = 'author'> criado por <a href="https://github.com/mateus00dias" target = "_blank" rel = "noreferrer" className = 'author'><ins>Mateus Dias🍺</ins></a></span>
             </footer>
         </div>
     )
